5ad03102f4223e6e9fd0f3362c77a7fe1a1dcd38
Rithish Kesav Saravanan <rithish_saravanan@mohawkind.com>
2023-04-09 16:57:00 -0700

    result.html version 1 completed

--- QuizApp/Version 1/script.js
+++ QuizApp/Version 1/script.js
@@ -2,6 +2,10 @@ const resultText = document.getElementById("result-text");
 const restartButton = document.getElementById("restart-button");
 // const quizForm = document.getElementById("quiz-form");
 const quizForm = document.querySelector("quiz-form");
+const resultDetails = document.getElementById("result-details");
+const checkbox = document.getElementById("checkbox");
+const slider = document.getElementById("slider");
+const sliderValue = document.getElementById("slider-value");
 
 if (resultText) {
   const correctAnswer = "red";
@@ -22,6 +26,16 @@ if (restartButton) {
     location.href = "index.html";
   });
 }
+
+if (resultDetails && checkbox) {
+  checkbox.addEventListener("change", () => {
+    resultDetails.style.display = checkbox.checked ? "block" : "none";
+  });
+}
+
+slider.addEventListener("input", () => {
+  sliderValue.textContent = slider.value;
+});
 
 if (quizForm) {
   quizForm.addEventListener("submit", (e) => {
